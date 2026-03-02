81d059e83cd3c62828fd950d58b7c4b090fb5163
KauchPotato <sohrab@postliter8.com>
2026-03-02 15:10:21 +0200

    feat: bordering countries

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+async function getCountryByCode(countryCode){
+    try{
+        const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+        const data = await response.json();
+        const name = document.createElement('h2');
+        const flag = document.createElement('img');
+        name.innerHTML = data[0].name.common;
+        flag.src = data[0].flags.svg;
+        document.getElementById('bordering-countries').appendChild(name);
+        document.getElementById('bordering-countries').appendChild(flag);
+    } catch(error){
+        document.getElementById('error-message').innerHTML = error;
+    }
+}
+
 
 
 async function searchCountry(countryName){
@@ -6,9 +21,13 @@ async function searchCountry(countryName){
         document.getElementById('bordering-countries').innerHTML = '';
         const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
         const data = await response.json();
-        
+        const borders = data[0].borders;
         const country = data[0];
-
+        if(borders){
+            borders.forEach(element => {
+            getCountryByCode(element);
+        });
+        }
         document.getElementById('country-info').innerHTML = `
         <h2>${country.name.common}</h2>
         <p><strong>Capital:</strong> ${country.capital[0]}</p>
