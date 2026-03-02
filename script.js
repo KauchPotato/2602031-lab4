cb3b22dbd479e441b4081454c3e716ff319f5ee0
KauchPotato <sohrab@postliter8.com>
2026-03-02 15:09:47 +0200

    feat : searchCountry functionality

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+
+
 async function searchCountry(countryName){
     try{
-        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById('loading-spinner').classList.remove("hidden");
+        document.getElementById('bordering-countries').innerHTML = '';
         const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
         const data = await response.json();
-        const borders = data[0].borders;
-        console.log(borders);
+        
+        const country = data[0];
+
+        document.getElementById('country-info').innerHTML = `
+        <h2>${country.name.common}</h2>
+        <p><strong>Capital:</strong> ${country.capital[0]}</p>
+        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+        <p><strong>Region:</strong> ${country.region}</p>
+        <img src="${country.flags.svg}" alt="${country.name.common} flag">`;
         
     } catch(error){
-        document.getElementById("error-message").innerHTML(error);
+        document.getElementById("error-message").innerHTML = error;
     } finally {
-        document.getElementById("spinner").classList.add("hidden");
+        document.getElementById('loading-spinner').classList.add("hidden");
     }
 }
+
+
 
 document.getElementById('search-btn').addEventListener('click',() => {
     const country = document.getElementById('country-input').value;
